451df979fe73cb1d7f28f5469389c15fbd9304cf
Oleg Kalnichevski <olegk@apache.org>
2009-07-01 20:02:21 +0000

    Added basic SMTPContent implementations

---
+++ src/main/java/com/ok2c/lightmtp/message/content/ByteArraySource.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.ok2c.lightmtp.message.content;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+import com.ok2c.lightmtp.message.SMTPContent;
+
+public class ByteArraySource implements SMTPContent<ReadableByteChannel> {
+
+	private final byte[] content;
+	
+	private InputStream stream;
+	
+	public ByteArraySource(final byte[] content) {
+		super();
+		this.content = content;
+		this.stream = null;
+	}
+	
+	public ReadableByteChannel channel() {
+		if (this.stream == null) {
+			this.stream = new ByteArrayInputStream(this.content);
+		}
+		return Channels.newChannel(this.stream);
+	}
+
+	public void finish() {
+		this.stream = null;
+	}
+	
+}
---
+++ src/main/java/com/ok2c/lightmtp/message/content/FileSource.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.ok2c.lightmtp.message.content;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.ReadableByteChannel;
+
+import com.ok2c.lightmtp.message.SMTPContent;
+
+public class FileSource implements SMTPContent<ReadableByteChannel> {
+
+	private final File file;
+
+	private RandomAccessFile rafile;
+	
+	public FileSource(final File file) {
+		super();
+		this.file = file;
+		this.rafile = null;
+	}
+	
+	public ReadableByteChannel channel() throws FileNotFoundException {
+		if (this.rafile == null) {
+			this.rafile = new RandomAccessFile(this.file, "r");
+		}
+		return this.rafile.getChannel();
+	}
+
+	public void finish() {
+		if (this.rafile != null) {
+			try {
+				this.rafile.close();
+			} catch (IOException ignore) {
+			}
+		}
+		this.rafile = null;
+	}
+	
+}
