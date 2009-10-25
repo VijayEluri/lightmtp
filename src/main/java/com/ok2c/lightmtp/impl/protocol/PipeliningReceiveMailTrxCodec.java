c43f863039da76550dbafb1864d8fc4dd801b075
oleg <olegk@apache.org>
2009-10-25 20:17:25 +0000

    Initial implementation of the server side SMTP codecs

---
+++ src/main/java/com/ok2c/lightmtp/impl/protocol/PipeliningReceiveMailTrxCodec.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.ok2c.lightmtp.impl.protocol;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.ok2c.lightmtp.SMTPCommand;
+import com.ok2c.lightmtp.SMTPProtocolException;
+import com.ok2c.lightmtp.SMTPReply;
+import com.ok2c.lightmtp.message.SMTPCommandParser;
+import com.ok2c.lightmtp.message.SMTPMessageParser;
+import com.ok2c.lightmtp.message.SMTPMessageWriter;
+import com.ok2c.lightmtp.message.SMTPReplyWriter;
+import com.ok2c.lightmtp.protocol.ProtocolHandler;
+import com.ok2c.lightmtp.protocol.ProtocolCodec;
+import com.ok2c.lightmtp.protocol.ProtocolCodecs;
+import com.ok2c.lightnio.IOSession;
+import com.ok2c.lightnio.SessionInputBuffer;
+import com.ok2c.lightnio.SessionOutputBuffer;
+
+public class PipeliningReceiveMailTrxCodec implements ProtocolCodec<ServerSessionState> {
+
+    private final ProtocolHandler<ServerSessionState> commandHandler;
+    private final SMTPMessageParser<SMTPCommand> parser;
+    private final SMTPMessageWriter<SMTPReply> writer;
+    private final Queue<SMTPReply> pendingReplies;
+
+    private boolean idle;
+    private boolean completed;
+
+    public PipeliningReceiveMailTrxCodec(final ProtocolHandler<ServerSessionState> commandHandler) {
+        super();
+        if (commandHandler == null) {
+            throw new IllegalArgumentException("Command handler may not be null");
+        }
+        this.commandHandler = commandHandler;
+        this.parser = new SMTPCommandParser();
+        this.writer = new SMTPReplyWriter();
+        this.pendingReplies = new LinkedList<SMTPReply>();
+        this.idle = true;
+        this.completed = false;
+    }
+
+    public void reset(
+            final IOSession iosession,
+            final ServerSessionState sessionState) throws IOException, SMTPProtocolException {
+        this.parser.reset();
+        this.writer.reset();
+        this.pendingReplies.clear();
+        this.idle = true;
+        this.completed = false;
+    }
+
+    public void produceData(
+            final IOSession iosession,
+            final ServerSessionState sessionState) throws IOException, SMTPProtocolException {
+        if (iosession == null) {
+            throw new IllegalArgumentException("IO session may not be null");
+        }
+        if (sessionState == null) {
+            throw new IllegalArgumentException("Session state may not be null");
+        }
+
+        SessionOutputBuffer buf = sessionState.getOutbuf();
+
+        while (!this.pendingReplies.isEmpty()) {
+            SMTPReply reply = this.pendingReplies.remove();
+            this.writer.write(reply, buf);
+        }
+
+        if (buf.hasData()) {
+            buf.flush(iosession.channel());
+        }
+        if (!buf.hasData()) {
+            if (sessionState.getDataType() != null) {
+                this.completed = true;
+            }
+            iosession.clearEvent(SelectionKey.OP_WRITE);
+        }
+    }
+
+    public void consumeData(
+            final IOSession iosession,
+            final ServerSessionState sessionState) throws IOException, SMTPProtocolException {
+        if (iosession == null) {
+            throw new IllegalArgumentException("IO session may not be null");
+        }
+        if (sessionState == null) {
+            throw new IllegalArgumentException("Session state may not be null");
+        }
+
+        SessionInputBuffer buf = sessionState.getInbuf();
+
+        int bytesRead = buf.fill(iosession.channel());
+        SMTPCommand command = this.parser.parse(buf, bytesRead == -1);
+
+        if (command != null) {
+            SMTPReply reply = this.commandHandler.handle(command, sessionState);
+            this.pendingReplies.add(reply);
+            iosession.setEvent(SelectionKey.OP_WRITE);
+
+            this.idle = (sessionState.getSender() == null);
+        }
+
+        if (bytesRead == -1) {
+            throw new UnexpectedEndOfStreamException();
+        }
+    }
+
+    public boolean isIdle() {
+        return this.idle;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    public String next(
+            final ProtocolCodecs<ServerSessionState> codecs,
+            final ServerSessionState sessionState) {
+        if (isCompleted()) {
+            return ProtocolState.DATA.name();
+        } else {
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/com/ok2c/lightmtp/impl/protocol/ServiceReadyCodec.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.ok2c.lightmtp.impl.protocol;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+
+import com.ok2c.lightmtp.SMTPCodes;
+import com.ok2c.lightmtp.SMTPProtocolException;
+import com.ok2c.lightmtp.SMTPReply;
+import com.ok2c.lightmtp.message.SMTPMessageWriter;
+import com.ok2c.lightmtp.message.SMTPReplyWriter;
+import com.ok2c.lightmtp.protocol.ProtocolCodec;
+import com.ok2c.lightmtp.protocol.ProtocolCodecs;
+import com.ok2c.lightnio.IOSession;
+import com.ok2c.lightnio.SessionOutputBuffer;
+
+public class ServiceReadyCodec implements ProtocolCodec<ServerSessionState> {
+
+    private final SMTPMessageWriter<SMTPReply> writer;
+
+    private SMTPReply pendingReply;
+    private boolean completed;
+
+    public ServiceReadyCodec() {
+        super();
+        this.writer = new SMTPReplyWriter();
+    }
+
+    public void reset(
+            final IOSession iosession,
+            final ServerSessionState sessionState) throws IOException, SMTPProtocolException {
+        this.writer.reset();
+        this.pendingReply = new SMTPReply(SMTPCodes.SERVICE_READY,
+                sessionState.getServerId() + " service ready");
+        this.completed = false;
+
+        iosession.setEventMask(SelectionKey.OP_WRITE);
+    }
+
+    public void produceData(
+            final IOSession iosession,
+            final ServerSessionState sessionState) throws IOException, SMTPProtocolException {
+        if (iosession == null) {
+            throw new IllegalArgumentException("IO session may not be null");
+        }
+        if (sessionState == null) {
+            throw new IllegalArgumentException("Session state may not be null");
+        }
+
+        SessionOutputBuffer buf = sessionState.getOutbuf();
+
+        if (this.pendingReply != null) {
+            this.writer.write(this.pendingReply, buf);
+            this.pendingReply = null;
+        }
+
+        if (buf.hasData()) {
+            buf.flush(iosession.channel());
+        }
+        if (!buf.hasData()) {
+            this.completed = true;
+            iosession.setEventMask(SelectionKey.OP_READ);
+        }
+    }
+
+    public void consumeData(
+            final IOSession iosession,
+            final ServerSessionState sessionState) throws IOException, SMTPProtocolException {
+    }
+
+    public boolean isIdle() {
+        return this.completed;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    public String next(
+            final ProtocolCodecs<ServerSessionState> codecs,
+            final ServerSessionState sessionState) {
+        if (isCompleted()) {
+            return ProtocolState.MAIL.name();
+        } else {
+            return null;
+        }
+    }
+
+}
