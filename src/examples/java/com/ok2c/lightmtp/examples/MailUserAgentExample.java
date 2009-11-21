8d8300e258f5cf3bdf38a542a460133b3a7e8220
Oleg Kalnichevski <olegk@apache.org>
2009-11-21 00:42:54 +0000

    Mail User Agent API examples

---
+++ src/examples/java/com/ok2c/lightmtp/examples/MailUserAgentExample.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.ok2c.lightmtp.examples;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Future;
+
+import com.ok2c.lightmtp.agent.SMTPProtocol;
+import com.ok2c.lightmtp.impl.agent.DefaultMailUserAgent;
+import com.ok2c.lightmtp.message.content.ByteArraySource;
+import com.ok2c.lightmtp.protocol.BasicDeliveryRequest;
+import com.ok2c.lightmtp.protocol.DeliveryRequest;
+import com.ok2c.lightmtp.protocol.DeliveryResult;
+import com.ok2c.lightnio.impl.IOReactorConfig;
+
+public class MailUserAgentExample {
+
+    public static void main(String[] args) throws Exception {
+
+        String text1 = new String(
+                "From: root\r\n" +
+                "To: testuser1\r\n" +
+                "Subject: test message 1\r\n" +
+                "\r\n" +
+                "This is a short test message 1\r\n");
+        String text2 = new String(
+                "From: root\r\n" +
+                "To: testuser1, testuser2\r\n" +
+                "Subject: test message 2\r\n" +
+                "\r\n" +
+                "This is a short test message 2\r\n");
+        String text3 = new String(
+                "From: root\r\n" +
+                "To: testuser1, testuser2, testuser3\r\n" +
+                "Subject: test message 3\r\n" +
+                "\r\n" +
+                "This is a short test message 3\r\n");
+
+        List<DeliveryRequest> requests = new ArrayList<DeliveryRequest>();
+        requests.add(new BasicDeliveryRequest(
+                "root", 
+                Arrays.asList("testuser1"), 
+                new ByteArraySource(text1.getBytes("US-ASCII"))));
+        requests.add(new BasicDeliveryRequest(
+                "root", 
+                Arrays.asList("testuser1", "testuser2"), 
+                new ByteArraySource(text2.getBytes("US-ASCII"))));
+        requests.add(new BasicDeliveryRequest(
+                "root", 
+                Arrays.asList("testuser1", "testuser2", "testuser3"), 
+                new ByteArraySource(text3.getBytes("US-ASCII"))));
+
+        
+        DefaultMailUserAgent mua = new DefaultMailUserAgent(
+                SMTPProtocol.LMTP, new IOReactorConfig());
+        mua.start();
+        
+        try {
+
+            InetSocketAddress address = new InetSocketAddress("localhost", 2525);
+            
+            Queue<Future<DeliveryResult>> queue = new LinkedList<Future<DeliveryResult>>();
+            for (DeliveryRequest request: requests) {
+                queue.add(mua.deliver(address, request));
+            }
+
+            while (!queue.isEmpty()) {
+                Future<DeliveryResult> future = queue.remove();
+                DeliveryResult result = future.get();
+                System.out.println("Delivery result: " + result);
+            }
+            
+        } finally {
+            mua.shutdown();
+        }
+    }
+    
+}
---
+++ src/examples/java/com/ok2c/lightmtp/examples/SendMailExample.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.ok2c.lightmtp.examples;
+
+import java.io.File;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.concurrent.Future;
+
+import com.ok2c.lightmtp.agent.SMTPProtocol;
+import com.ok2c.lightmtp.impl.agent.DefaultMailUserAgent;
+import com.ok2c.lightmtp.message.content.FileSource;
+import com.ok2c.lightmtp.protocol.BasicDeliveryRequest;
+import com.ok2c.lightmtp.protocol.DeliveryRequest;
+import com.ok2c.lightmtp.protocol.DeliveryResult;
+import com.ok2c.lightnio.impl.IOReactorConfig;
+
+public class SendMailExample {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 3) {
+            System.out.println("Usage: sender recipient1[;recipient2;recipient3;...] file");
+            System.exit(0);
+        }
+        
+        String sender = args[0];
+        List<String> recipients = new ArrayList<String>();
+        StringTokenizer tokenizer = new StringTokenizer(args[1], ";");
+        while (tokenizer.hasMoreTokens()) {
+            String s = tokenizer.nextToken();
+            s = s.trim();
+            if (s.length() > 0) {
+                recipients.add(s);
+            }
+        }
+        
+        File src = new File(args[2]);
+        if (!src.exists()) {
+            System.out.println("File '" + src + "' does not exist");
+            System.exit(0);
+        }
+        
+        DeliveryRequest request = new BasicDeliveryRequest(sender, recipients, new FileSource(src));
+        
+        DefaultMailUserAgent mua = new DefaultMailUserAgent(
+                SMTPProtocol.LMTP, new IOReactorConfig());
+        mua.start();
+        
+        try {
+            
+            InetSocketAddress address = new InetSocketAddress("localhost", 2525);
+            
+            Future<DeliveryResult> future = mua.deliver(address, request);
+            
+            DeliveryResult result = future.get();
+            System.out.println("Delivery result: " + result);
+            
+        } finally {
+            mua.shutdown();
+        }
+    }
+    
+}
